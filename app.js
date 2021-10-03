4efef1912515bd18d473f91df304ddd734c6be12
Limitless-Rakib <80609225+Rakib-Coder00@users.noreply.github.com>
2021-10-03 23:42:28 +0600

    feature added

--- app.js
+++ app.js
@@ -1,47 +1,79 @@
 
 console.log('Welcome to note app');
 
-
+// if user add note ==>
 let addBtn = document.getElementById('addBtn')
 
-addBtn.addEventListener('click', function(e){
-    let notes = localStorage.getItem('notes')
+addBtn.addEventListener('click', (e)=>{
     let inputTxt = document.getElementById('inputTxt')
-
+    let notes = localStorage.getItem('notes')
     if (notes == null) {
-        notesObj = [ ]
+        notesObj = []
     }
-    else {
+    else{
         notesObj = JSON.parse(notes)
     }
     notesObj.push(inputTxt.value)
     localStorage.setItem('notes', JSON.stringify(notesObj))
-    inputTxt.value = ''
+    inputTxt.value = ' '
     console.log(notesObj);
+
     showNotes()
 })
+// function to show elements 
 
 function showNotes() {
     let notes = localStorage.getItem('notes')
-    if (notes === null) {
+    if (notes == null) {
         notesObj = []
     }
-    else {
+    else{
         notesObj = JSON.parse(notes)
     }
     let html = ''
-    notesObj.forEach(function(element,  index) {
+    notesObj.forEach((element, index) => {
         html += `
         <div class="card my-2 mx-2" style="width: 18rem;">
                 <div class="card-body">
-                    <h5 class="card-title">Note ${index + 1} title</h5>
+                    <h5 class="card-title">Note title ${index}</h5>
                     <p class="card-text">${element}</p>
-                    <button href="" class="btn btn-danger">Delete Note</button>
+                    <button id = "${index}" onclick = 'deleteNote(this.id)'  class="btn btn-danger">Delete Note</button>
                 </div>
-       </div>`
+            </div>
+        `
     });
-    let notesElm = document.getElementById('notes')
-    if (notes.length != 0) {
-        notesElm.innerHTML = html;
+
+    let notesElm = document.getElementById('note')
+    if (notesObj.length != 0) {
+        notesElm.innerHTML = html
+    }
+    else{
+        notesElm.innerHTML = `Write a Note.`
     }
 }
+
+// function to delete note
+
+function deleteNote(index) {
+    console.log('im deleting', index);
+
+    let notes = localStorage.getItem('notes')
+    if (notes == null) {
+        notesObj = []
+    }
+    else{
+        notesObj = JSON.parse(notes)
+    }
+
+    notesObj.splice(index, 1)
+    localStorage.setItem('notes', JSON.stringify(notesObj))
+    showNotes()
+}
+
+
+let search = document.getElementById('searchTxt')
+search.addEventListener('input', () =>{
+    let inpVal = search.value
+    console.log('input', inpVal);
+    let noteCard = document.getElementsByClassName
+})
